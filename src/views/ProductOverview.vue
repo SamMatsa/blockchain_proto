45b80f09dfb025f1c3e5900eda36a4c394b06286
Sam Matsa <SamMatsa>
2020-03-29 22:44:34 +0200

    vue.config.js added (proxy) and get request added to ProdcutOverview

--- src/views/ProductOverview.vue
+++ src/views/ProductOverview.vue
@@ -7,10 +7,38 @@
 </template>
 
 <script>
-import ProductDetail from '../components/ProductDetail'
+import ProductDetail from '../components/ProductDetail';
+import axios from 'axios';
+
 export default {
     components:{
         ProductDetail
+    },
+    data(){
+      return{
+        list: []
+      }
+    },
+    methods:{
+      getList(){
+        axios.get(
+          'http://localhost:8080/getProducts',
+          {withCredentials: true,
+          headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+          },{
+            auth: {
+              username: 'vsapiuser',
+              password: 'BejB75sV'
+          }}).then(function(response){
+            console.log(response);
+          })
+      }
+    },
+    mounted(){
+      this.getList();
     }
 }
 </script>
