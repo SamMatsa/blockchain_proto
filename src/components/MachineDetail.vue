360f04c8346aed42ebcbbba5761ee18c23057c92
niklaaaas98 <niklas.hauschel@web.de>
2020-03-30 16:16:27 +0200

    Added Machine from API

--- src/components/MachineDetail.vue
+++ src/components/MachineDetail.vue
@@ -1,27 +1,74 @@
 <template>
     <div>
-  <b-card
-    title="Machine XYZ"
-    img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIyZk8xeEQgaRz2BlnyS9kijaRj6_5LMlZnW4FpdwaKsNT6kNr"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Bohren | Schweißen | Schrauben
+      <b-col>
+    <b-card
+      img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRIyZk8xeEQgaRz2BlnyS9kijaRj6_5LMlZnW4FpdwaKsNT6kNr"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+     
+    >
+    <b-card-title>
+      {{name}}
+      </b-card-title>
+    <b-card-text v-for="(value) in MachineTask" v-bind:key="value.id">
+        {{ value.description }},  {{ value.duration }}s
     </b-card-text>
 
- <div>
-  <b-dropdown id="dropdown-buttons" text="Aktuell: Produkt ABC" class="m-2">
-    <b-dropdown-item-button>Produkt 1 | Schrauben | 12:44</b-dropdown-item-button>
-    <b-dropdown-item-button>Produkt 2 | Schweißen | 12:32</b-dropdown-item-button>
-    <b-dropdown-item-button>Produkt 3 | Schrauben | 12:20</b-dropdown-item-button>
-    <b-dropdown-item-button>Produkt 4 | Schrauben | 12:08</b-dropdown-item-button>
-  </b-dropdown>
-</div> 
-  </b-card>
+    <div>
+    <b-dropdown id="dropdown-buttons" text="Aktuell: Produkt ABC" class="m-2">
+      <b-dropdown-item-button>Produkt 1 | Schrauben | 12:44</b-dropdown-item-button>
+      <b-dropdown-item-button>Produkt 2 | Schweißen | 12:32</b-dropdown-item-button>
+      <b-dropdown-item-button>Produkt 3 | Schrauben | 12:20</b-dropdown-item-button>
+      <b-dropdown-item-button>Produkt 4 | Schrauben | 12:08</b-dropdown-item-button>
+    </b-dropdown>
 
+  </div> 
+  </b-card>
+</b-col>
 </div>
 </template>
+
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: 'MachineDetail',
+    props: {
+        id: Number,
+        name: String,
+    }, 
+    data: () => ({
+        MachineTask: []
+    }), 
+    mounted() {
+      this.getTasks(this.id)
+    },
+    methods: {
+      getTasks(index) {
+       console.log(index)
+        var user = "vsapiuser";
+        var pass = "BejB75sV";
+        // var url = 'http://localhost:8081/getTaks';
+        var url = 'https://vsapi.wegmann.dev/getTasks';
+
+        var authorizationBasic = window.btoa(user + ':' + pass);
+        var config = {
+          "headers":{
+            "Authorization": "Basic " + authorizationBasic
+          }
+        };
+
+        axios.get(url, config).then(response => {
+          this.MachineTask = response.data
+          console.log("Machine Tasks",this.MachineTask)
+        })
+        }
+      
+    }
+}
+</script>
--- src/views/MachineOverview.vue
+++ src/views/MachineOverview.vue
@@ -1,18 +1,59 @@
 <template>
 <b-container>
   <b-row align-v="center">
-    <b-col><MachineDetail /></b-col>
-    <b-col><MachineDetail /></b-col>
-    <b-col><MachineDetail /></b-col>
+    <MachineDetail v-for="(data, index) in theList" v-bind:key="data.id" v-bind:name="data.name"  :id="data.id" :getTasks="(ev) => getTasks(index)"/>
   </b-row>
 </b-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 import MachineDetail from '../components/MachineDetail'
 export default {
-    components:{
-        MachineDetail
+  data() {
+   return {
+  options: [],
+    theList: [],
+    new: [],
+    errors: [],
+    url: 'https://vsapi.wegmann.dev/getMachines',
+    username: 'vsapiuser',
+    password: 'BejB75sV',
+    token: Buffer.from(`${this.username}:${this.password}`, 'utf8').toString('base64'),
+   }
+  },
+  components:{
+      MachineDetail
+  }, 
+  methods: {
+    getMachines(){
+      console.log("Test")
+      
+        var user = "vsapiuser";
+        var pass = "BejB75sV";
+        // var url = 'http://localhost:8083/getMachines';
+        var url = 'https://vsapi.wegmann.dev/getMachines';
+
+        var authorizationBasic = window.btoa(user + ':' + pass);
+        var config = {
+          "headers":{
+            "Authorization": "Basic " + authorizationBasic
+          }
+        };
+
+        axios.get(url, config).then(response => {
+          this.theList = response.data
+          console.log(this.theList);
+        })
+    
+    },
+    
+
+    },
+    mounted(){
+      this.getMachines();
     }
 }
+
 </script>
