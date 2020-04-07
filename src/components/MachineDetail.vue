7f27179f359440605233f9836f7ea92bfc083c44
niklaaaas98 <niklas.hauschel@web.de>
2020-04-07 10:36:33 +0200

    Updated code

--- src/components/MachineDetail.vue
+++ src/components/MachineDetail.vue
@@ -7,24 +7,31 @@
           img-top
           tag="article"
           class="mb-2"
-          style="max-width: 20rem;"
-        >
+          style="max-width: 20rem;">
         <b-card-title>
           {{name}}
-          </b-card-title>
-        <b-card-text v-for="(value) in MachineTask" v-bind:key="value.id">
+        </b-card-title>
+        <b-card-text v-for="(value) in tasks" v-bind:key="value.id">
             {{ value.description }},  {{ value.duration }}s
         </b-card-text>
 
         <div>
-          <div v-if="!newList">
+          <div v-if="!transactions">
             <b-button v-b-modal.modal-scrollable v-on:click="onShowModal" disable>No product planned</b-button>
           </div>
-          <div v-if="newList">
+          <div v-if="transactions">
             <b-button v-b-modal.modal-scrollable v-on:click="onShowModal">Todo:current Product</b-button>
 
-            <b-modal id="modal-scrollable" size="xl" v-if="showModal"  @close="showModal=false" @cancel="showModal=false" no-close-on-backdrop  @ok="showModal=false" scrollable title="Scrollable Content">
-                  <b-table striped hover :items="MachineTransaction"></b-table>
+            <b-modal id="modal-scrollable" 
+            size="xl" 
+            v-if="showModal"  
+            @close="showModal=false" 
+            @cancel="showModal=false" 
+            no-close-on-backdrop  
+            @ok="showModal=false" 
+            scrollable 
+            title= "Transactions:">
+                  <b-table striped hover :items="transactions"></b-table>
             </b-modal>
           </div>
       </div> 
@@ -36,71 +43,19 @@
 
 <script>
 
-
-
-import axios from 'axios'
-
 export default {
     name: 'MachineDetail',
     props: {
         id: Number,
         name: String,
+        tasks: Array,
+        transactions: Array
     }, 
     data: () => ({
-        MachineTask: [],
-        newList: [],
-        MachineTransaction: [],
         showModal: false
     }), 
-    mounted() {
-      this.getTasks(this.id)
-      this.getTransactions(this.id)
-      setInterval(() => {
-        this.getTransactions(this.id) 
-        // location.reload();
-        console.log("Refresh!")
-      }, 10000);
-      
-    },
     methods: {
-      getTasks(index) {
-        var user = "vsapiuser";
-        var pass = "BejB75sV";
-        var url = 'https://vsapi.wegmann.dev/getTasks';
-
-        var authorizationBasic = window.btoa(user + ':' + pass);
-        var config = {
-          "headers":{
-            "Authorization": "Basic " + authorizationBasic
-          }
-        };
-      var newUrl = url + "?machine_id="+index
-        axios.get(newUrl, config).then(response => {
-          this.MachineTask = response.data
-        })
-        },
-      getTransactions(index) {
-        var user = "vsapiuser";
-        var pass = "BejB75sV";
-        var url = 'https://vsapi.wegmann.dev/getTransactions';
-
-        var authorizationBasic = window.btoa(user + ':' + pass);
-        var config = {
-          "headers":{
-            "Authorization": "Basic " + authorizationBasic
-          }
-        };
-      var newUrl = url + "?machine_id="+index
-      axios.get(newUrl, config).then(response => {
-        this.newList = []
-        for(var i=0; i < response.data.length; i++) {
-          var response1 = response.data[i]
-          this.newList.push({Product: response1.product.name, Task: response1.task.description, End: response1.end, Begin: response1.begin, Status: response1.status})
-        }
-        this.MachineTransaction = this.newList
-
-      })
-      },
+     
       onShowModal(){
           this.showModal = true;
       },
--- src/views/MachineOverview.vue
+++ src/views/MachineOverview.vue
@@ -1,7 +1,7 @@
 <template>
 <b-container>
   <b-row align-v="center">
-    <MachineDetail v-for="(data, index) in theList" v-bind:key="data.id" v-bind:name="data.name"  :id="data.id" :getTasks="(ev) => getTasks(index)"/>
+    <MachineDetail v-for="(data) in theList" v-bind:key="data.id" v-bind:name="data.name" v-bind:transactions="data.transactions" v-bind:tasks="data.tasks" :id="data.id" />
   </b-row>
 </b-container>
 </template>
@@ -27,31 +27,48 @@ export default {
       MachineDetail
   }, 
   methods: {
-    getMachines(){
-      
+    async getMachines(){
+        var responseRequest = await this.sendRequest("getMachines");
+        for(var i=0; i<responseRequest.length; i++) {
+          console.log("id",responseRequest[i].id)
+          var responseRequest2 = await this.sendRequest("getTransactions?machine_id="+responseRequest[i].id);
+            
+          var newList = []
+            for(var j=0; j < responseRequest2.length; j++) {
+              var response1 = responseRequest2[j]
+              newList.push({Product: response1.product.name, Task: response1.task.description, End: response1.end, Begin: response1.begin, Status: response1.status})
+            }
+          responseRequest[i].transactions = newList
+        }
+         this.theList = responseRequest
+    },
+
+
+    test() {
+      return "hello"
+    }, 
+     async sendRequest(endpoint) {
+        var ep = endpoint;
         var user = "vsapiuser";
         var pass = "BejB75sV";
-        // var url = 'http://localhost:8083/getMachines';
-        var url = 'https://vsapi.wegmann.dev/getMachines';
-
+        var url = `https://vsapi.wegmann.dev/${ep}`;
         var authorizationBasic = window.btoa(user + ':' + pass);
         var config = {
           "headers":{
             "Authorization": "Basic " + authorizationBasic
           }
         };
-
-        axios.get(url, config).then(response => {
-          this.theList = response.data
-          // console.log(this.theList);
-        })
-    
-    },
-    
-
-    },
+        var res = await axios.get(url, config);
+        this.lastResponseObject = res.data;
+        return res.data;
+      },
+  },
     mounted(){
       this.getMachines();
+      setInterval(() => {
+        console.log("Refresh!")
+        this.getMachines();
+      }, 10000);
     }
 }
 
